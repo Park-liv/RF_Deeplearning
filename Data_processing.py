d4b92cb210759ff7311e11b97cb2a5e22c68b2f4
Park-liv <52243152+Park-liv@users.noreply.github.com>
2020-03-06 04:51:44 +0900

    Add files via upload

--- Data_processing.py
+++ Data_processing.py
@@ -1,16 +1,6 @@
 import numpy as np
 import struct
 
-TR1 = open('rf_433_01', 'rb')
-TR2 = open('rf_433_02', 'rb')
-TR3 = open('rf_433_03', 'rb')
-TR4 = open('rf_433_04', 'rb')
-TR5 = open('rf_433_05', 'rb')
-TR6 = open('rf_433_06', 'rb')
-TR7 = open('rf_433_07', 'rb')
-TR8 = open('rf_433_08', 'rb')
-TR9 = open('rf_433_09', 'rb')
-
 # convert bytefile to floatfile & reshape
 def CRS_data(bytefile):
     bytefile.seek(0, 2)
@@ -47,50 +37,70 @@ def IL_data(filename, num_label, period=1999):
     array_data = np.array(data)
     return array_data
 
+TR1 = open('rf_433_01', 'rb')
+TR2 = open('rf_433_01', 'rb')
+TR3 = open('rf_433_01', 'rb')
+TR4 = open('rf_433_01', 'rb')
+TR5 = open('rf_433_01', 'rb')
+TR6 = open('rf_433_01', 'rb')
+TR7 = open('rf_433_01', 'rb')
+TR8 = open('rf_433_01', 'rb')
+TR9 = open('rf_433_01', 'rb')
 
 converted_data_1 = CRS_data(TR1)
-data_001 = IL_data(converted_data_1, 1)
-
+data_001 = IL_data(converted_data_1, 0)
+print(data_001.shape)
 converted_data_2 = CRS_data(TR2)
-data_002 = IL_data(converted_data_2, 2)
-
+data_002 = IL_data(converted_data_2, 1)
+print(data_002.shape)
 converted_data_3 = CRS_data(TR3)
-data_003 = IL_data(converted_data_3, 3)
-
+data_003 = IL_data(converted_data_3, 2)
+print(data_003.shape)
 converted_data_4 = CRS_data(TR4)
-data_004 = IL_data(converted_data_4, 4)
-
+data_004 = IL_data(converted_data_4, 3)
+print(data_004.shape)
 converted_data_5 = CRS_data(TR5)
-data_005 = IL_data(converted_data_5, 5)
-
+data_005 = IL_data(converted_data_5, 4)
+print(data_005.shape)
 converted_data_6 = CRS_data(TR6)
-data_006 = IL_data(converted_data_6, 6)
-
+data_006 = IL_data(converted_data_6, 5)
+print(data_006.shape)
 converted_data_7 = CRS_data(TR7)
-data_007 = IL_data(converted_data_7, 7)
-
+data_007 = IL_data(converted_data_7, 6)
+print(data_007.shape)
 converted_data_8 = CRS_data(TR8)
-data_008 = IL_data(converted_data_8, 8)
-
+data_008 = IL_data(converted_data_8, 7)
+print(data_008.shape)
 converted_data_9 = CRS_data(TR9)
-data_009 = IL_data(converted_data_9, 9)
+data_009 = IL_data(converted_data_9, 8)
+print(data_009.shape)
+'''
+1번 29739개
+2번 22255개
+3번 22400개
+4번 23667개
+5번 24114개
+6번 21688개
+7번 22498개
+8번 22287개
+9번 21564개 
+'''
 
-# print(len(converted_data_0))
-# print(data_000.shape)
-# print(data_000[23, 1990:2001])
-#
-# print(len(converted_data_1))
-# print(data_001.shape)
-# print(data_001[23, 1990:2001])
-#
-# print(len(converted_data_2))
-# print(data_002.shape)
-# print(data_002[23, 1990:2001])
-
-combined_data = np.concatenate((data_001, data_002, data_003, data_004, data_005, data_006, data_007, data_008, data_009), axis=0)
+combined_data = np.concatenate((data_001[:20000,:],
+                                data_002[:20000,:],
+                                data_003[:20000,:],
+                                data_004[:20000,:],
+                                data_005[:20000,:],
+                                data_006[:20000,:],
+                                data_007[:20000,:],
+                                data_008[:20000,:],
+                                data_009[:20000,:]), axis=0)
 print(combined_data.shape)
 
-for i in range(5):
+num_shuffle = 10
+for i in range(num_shuffle):
     np.random.shuffle(combined_data)
 
-np.save('processed_data', combined_data)
+np.save('processed_data_1', combined_data)
+
+print("Data is shuffled {}times, processed and saved!".format(num_shuffle))
--- Nn_1.py
+++ Nn_1.py
@@ -2,6 +2,8 @@ import numpy as np
 import tensorflow as tf
 import math
 from batch_normalization import batch_norm
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 tf.set_random_seed(777)
 
@@ -15,8 +17,8 @@ test_Y_data = data[-num_test:, [-1]]
 print(train_X_data.shape, train_Y_data.shape, test_X_data.shape, test_Y_data.shape)
 
 nb_transmitter = 9
-batch_size = 1000
-num_epochs = 100
+batch_size = 10000
+num_epochs = 20
 num_iterations = int(math.ceil(train_X_data.shape[0] / batch_size))
 
 X = tf.placeholder(tf.float32, [None, 1999])
@@ -52,7 +54,7 @@ hypothesis = tf.matmul(L2, W3) + b3
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=hypothesis,
                                                                  labels=tf.stop_gradient([Y_one_hot])))
-train = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(cost)
+train = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost)
 
 prediction = tf.argmax(hypothesis, axis=1)
 correct_prediction = tf.equal(prediction, tf.argmax(Y_one_hot, axis=1))
@@ -61,9 +63,12 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
+    # epochList = []
+    # accList = []
     for epoch in range(num_epochs):
         avg_cost = 0
         avg_accuracy = 0
+
 
         for iteration in range(num_iterations):
             batch_xs = train_X_data[batch_size * iteration: batch_size * (iteration + 1), :]
@@ -73,6 +78,34 @@ with tf.Session() as sess:
             avg_accuracy += acc_val / num_iterations
 
         print(f"Epoch: {(epoch + 1):04d}, Cost: {avg_cost:.9f}, Accuracy: {avg_accuracy:.2%}")
+    #     epochList.append(epoch+1)
+    #     accList.append(avg_accuracy*100)
+    #
+    # plt.plot(epochList, accList)
+    # plt.xlabel("Epoch")
+    # plt.ylabel("Accuracy")
+    # plt.show()
 
     acc = sess.run(accuracy, feed_dict={X: test_X_data, Y: test_Y_data, keep_prob: 1})
     print(f"Accuracy: {(acc * 100):2.2f}%")
+
+    # show_X_data = test_X_data[-100:, :]
+    # show_Y_data = test_Y_data[-100:, :]
+    viewAcc = [[0] * nb_transmitter for i in range(nb_transmitter)]
+    #
+    pred = sess.run(prediction, feed_dict={X: test_X_data, keep_prob: 1})
+    for p, y in zip(pred, test_Y_data.flatten()):
+        # print("[{}] Prediction: {} True Y: {}".format(p == int(y), p, int(y)))
+        viewAcc[int(y)][p] += 1
+    # print(viewAcc)
+    # plt.figure(figsize=(9,9))
+    # sns.heatmap(data=viewAcc, annot=True, linewidths=.5, cmap='Blues')
+    for i in range(len(viewAcc)):
+            print(viewAcc[i],',')
+
+    # viewAcc = [[0] * nb_transmitter for i in range(nb_transmitter)]
+    # for i in range(test_X_data.shape[0]):
+    #     viewX = tf.reshape(test_X_data[i], [1, 1999])
+    #     pred = sess.run(prediction, feed_dict={X: viewX, keep_prob: 1})
+    #     viewAcc[test_Y_data[i,:]][pred] += 1
+    # print(viewAcc)
